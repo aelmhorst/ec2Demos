aba887847a03d7b3094b6bb59e1c283ac980b44d
Elmhorst, Andy <elmhorst@amazon.com>
2018-06-21 18:52:30 -0500

    added httpd-starter.sh

---
+++ httpd-starter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Apache 2.4
+yum update -y
+yum install httpd24 -y
+service httpd start
+chkconfig httpd on
+
+# Retrieve the EC2 instance ID and add it to the default httpd doc
+EC2ID=$(ec2metadata --instance-id)
+echo "<html><body><h1>Welcome to version 0 of EC2 Demo running on $EC2ID</h1></body></html>" > /var/www/html/index.html
