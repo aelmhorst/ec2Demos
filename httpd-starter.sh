8e878a0ddfe4ef392c03818297ee9297503d3d49
Elmhorst, Andy <elmhorst@amazon.com>
2018-06-21 19:27:30 -0500

    download ec2-metadata

--- httpd-starter.sh
+++ httpd-starter.sh
@@ -6,6 +6,10 @@ yum install httpd24 -y
 service httpd start
 chkconfig httpd on
 
+# Install the ec2-metadata tool
+wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
+chmod u+x ec2-metadata
+
 # Retrieve the EC2 instance ID and add it to the default httpd doc
-EC2ID=$(./ec2metadata --instance-id)
+EC2ID=$(./ec2-metadata --instance-id)
 echo "<html><body><h1>Welcome to version 0 of EC2 Demo running on $EC2ID</h1></body></html>" > /var/www/html/index.html
