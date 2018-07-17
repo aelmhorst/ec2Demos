fbf8612f4e89ba1b6c1e06f3b16f459ab9c71c4c
Elmhorst, Andy <elmhorst@amazon.com>
2018-07-17 17:47:06 -0500

    prettied up a bit

--- httpd-starter-amzn2.sh
+++ httpd-starter-amzn2.sh
@@ -15,4 +15,4 @@ chmod u+x ec2-metadata
 # Retrieve the EC2 instance ID and add it to the default httpd doc
 EC2ID=$(./ec2-metadata --instance-id)
 EC2OS=$(cat /etc/system-release)
-echo "<html><body><h1>Welcome to version 0 of EC2 Demo running on $EC2OS, instance: $EC2ID</h1></body></html>" > /var/www/html/index.html
+echo "<html><body><h1>Welcome to version 0 of EC2 Demo running on $EC2OS, $EC2ID</h1></body></html>" > /var/www/html/index.html
--- httpd-starter.sh
+++ httpd-starter.sh
@@ -12,4 +12,5 @@ chmod u+x ec2-metadata
 
 # Retrieve the EC2 instance ID and add it to the default httpd doc
 EC2ID=$(./ec2-metadata --instance-id)
-echo "<html><body><h1>Welcome to version 0 of EC2 Demo running on $EC2ID (Amazon Linux)</h1></body></html>" > /var/www/html/index.html
+EC2OS=$(cat /etc/system-release)
+echo "<html><body><h1>Welcome to version 0 of EC2 Demo running on $EC2OS, $EC2ID</h1></body></html>" > /var/www/html/index.html
