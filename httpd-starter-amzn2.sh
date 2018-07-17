ba1998c6a8fda5e26213f6b2f334df75d4888ab4
Elmhorst, Andy <elmhorst@amazon.com>
2018-07-17 17:28:47 -0500

    added support for amazon linux

---
+++ httpd-starter-amzn2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Updated starter script that uses systemd
+
+# Install Apache
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+
+# Install the ec2-metadata tool
+wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
+chmod u+x ec2-metadata
+
+# Retrieve the EC2 instance ID and add it to the default httpd doc
+EC2ID=$(./ec2-metadata --instance-id)
+echo "<html><body><h1>Welcome to version 0 of EC2 Demo running on $EC2ID</h1></body></html>" > /var/www/html/index.html
